9ff33ccdb185c414cdebe14b025f126387e17005
EtishaGarg <gargetisha@gmail.com>
2023-07-14 00:41:45 +0530

    Style, config & components changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        15: '60px',
+        1.25: '5px',
+      }
+    },
   },
   plugins: [],
 }
